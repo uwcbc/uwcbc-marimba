7266d27cdf1d8635bc00748af70cbd6024f822af
Nathan <Nathan@NATHAN>
2017-10-29 18:58:34 -0400

    Reformatted 'Create New Term' page

--- Marimba/newTerm.Designer.cs
+++ Marimba/newTerm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(newTerm));
             this.lblName = new System.Windows.Forms.Label();
             this.btnCreate = new System.Windows.Forms.Button();
             this.txtName = new System.Windows.Forms.TextBox();
@@ -52,18 +51,20 @@
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(185, 72);
+            this.lblName.Location = new System.Drawing.Point(356, 88);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(123, 18);
+            this.lblName.Size = new System.Drawing.Size(130, 20);
             this.lblName.TabIndex = 1;
             this.lblName.Text = "Name of Term";
             // 
             // btnCreate
             // 
             this.btnCreate.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreate.Location = new System.Drawing.Point(339, 512);
+            this.btnCreate.Location = new System.Drawing.Point(504, 673);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(126, 27);
+            this.btnCreate.Size = new System.Drawing.Size(168, 33);
             this.btnCreate.TabIndex = 11;
             this.btnCreate.Text = "Create Term";
             this.btnCreate.UseVisualStyleBackColor = true;
@@ -72,18 +73,20 @@
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(381, 69);
+            this.txtName.Location = new System.Drawing.Point(508, 85);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(241, 24);
+            this.txtName.Size = new System.Drawing.Size(320, 27);
             this.txtName.TabIndex = 2;
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Quicksand", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(328, 23);
+            this.lblTitle.Location = new System.Drawing.Point(504, 24);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(151, 18);
+            this.lblTitle.Size = new System.Drawing.Size(182, 22);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Create New Term";
             // 
@@ -91,16 +94,18 @@
             // 
             this.lblRehearsalStart.AutoSize = true;
             this.lblRehearsalStart.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRehearsalStart.Location = new System.Drawing.Point(574, 111);
+            this.lblRehearsalStart.Location = new System.Drawing.Point(903, 137);
+            this.lblRehearsalStart.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRehearsalStart.Name = "lblRehearsalStart";
-            this.lblRehearsalStart.Size = new System.Drawing.Size(175, 18);
+            this.lblRehearsalStart.Size = new System.Drawing.Size(184, 20);
             this.lblRehearsalStart.TabIndex = 3;
             this.lblRehearsalStart.Text = "Rehearsal Start Date";
             // 
             // mcRehearsalStart
             // 
             this.mcRehearsalStart.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mcRehearsalStart.Location = new System.Drawing.Point(542, 138);
+            this.mcRehearsalStart.Location = new System.Drawing.Point(859, 170);
+            this.mcRehearsalStart.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.mcRehearsalStart.MaxSelectionCount = 1;
             this.mcRehearsalStart.Name = "mcRehearsalStart";
             this.mcRehearsalStart.TabIndex = 4;
@@ -108,7 +113,8 @@
             // updNumRehearsals
             // 
             this.updNumRehearsals.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.updNumRehearsals.Location = new System.Drawing.Point(381, 341);
+            this.updNumRehearsals.Location = new System.Drawing.Point(509, 460);
+            this.updNumRehearsals.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.updNumRehearsals.Maximum = new decimal(new int[] {
             12,
             0,
@@ -120,7 +126,7 @@
             0,
             0});
             this.updNumRehearsals.Name = "updNumRehearsals";
-            this.updNumRehearsals.Size = new System.Drawing.Size(241, 24);
+            this.updNumRehearsals.Size = new System.Drawing.Size(321, 27);
             this.updNumRehearsals.TabIndex = 6;
             this.updNumRehearsals.Value = new decimal(new int[] {
             1,
@@ -132,9 +138,10 @@
             // 
             this.lblNumRehearsals.AutoSize = true;
             this.lblNumRehearsals.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumRehearsals.Location = new System.Drawing.Point(185, 343);
+            this.lblNumRehearsals.Location = new System.Drawing.Point(248, 462);
+            this.lblNumRehearsals.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNumRehearsals.Name = "lblNumRehearsals";
-            this.lblNumRehearsals.Size = new System.Drawing.Size(186, 18);
+            this.lblNumRehearsals.Size = new System.Drawing.Size(193, 20);
             this.lblNumRehearsals.TabIndex = 5;
             this.lblNumRehearsals.Text = "Number of Rehearsals";
             // 
@@ -142,18 +149,20 @@
             // 
             this.lblMembershipFee.AutoSize = true;
             this.lblMembershipFee.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMembershipFee.Location = new System.Drawing.Point(185, 382);
+            this.lblMembershipFee.Location = new System.Drawing.Point(248, 510);
+            this.lblMembershipFee.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMembershipFee.Name = "lblMembershipFee";
-            this.lblMembershipFee.Size = new System.Drawing.Size(143, 18);
+            this.lblMembershipFee.Size = new System.Drawing.Size(146, 20);
             this.lblMembershipFee.TabIndex = 7;
             this.lblMembershipFee.Text = "Membership Fee";
             // 
             // txtMembershipFee
             // 
             this.txtMembershipFee.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMembershipFee.Location = new System.Drawing.Point(381, 379);
+            this.txtMembershipFee.Location = new System.Drawing.Point(509, 506);
+            this.txtMembershipFee.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMembershipFee.Name = "txtMembershipFee";
-            this.txtMembershipFee.Size = new System.Drawing.Size(241, 24);
+            this.txtMembershipFee.Size = new System.Drawing.Size(320, 27);
             this.txtMembershipFee.TabIndex = 8;
             this.txtMembershipFee.Text = "20.00";
             // 
@@ -161,19 +170,21 @@
             // 
             this.lblOther.AutoSize = true;
             this.lblOther.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOther.Location = new System.Drawing.Point(185, 420);
+            this.lblOther.Location = new System.Drawing.Point(248, 557);
+            this.lblOther.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOther.Name = "lblOther";
-            this.lblOther.Size = new System.Drawing.Size(189, 54);
+            this.lblOther.Size = new System.Drawing.Size(195, 60);
             this.lblOther.TabIndex = 9;
             this.lblOther.Text = "Other Fees\r\n(enter one per line)\r\n(e.g. \"Uniform Fee,15\")";
             // 
             // txtOther
             // 
             this.txtOther.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOther.Location = new System.Drawing.Point(381, 417);
+            this.txtOther.Location = new System.Drawing.Point(509, 553);
+            this.txtOther.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtOther.Multiline = true;
             this.txtOther.Name = "txtOther";
-            this.txtOther.Size = new System.Drawing.Size(241, 76);
+            this.txtOther.Size = new System.Drawing.Size(320, 93);
             this.txtOther.TabIndex = 10;
             this.txtOther.Text = "Uniform Fee,15";
             // 
@@ -181,9 +192,10 @@
             // 
             this.lblTermStart.AutoSize = true;
             this.lblTermStart.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTermStart.Location = new System.Drawing.Point(73, 111);
+            this.lblTermStart.Location = new System.Drawing.Point(130, 137);
+            this.lblTermStart.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTermStart.Name = "lblTermStart";
-            this.lblTermStart.Size = new System.Drawing.Size(138, 18);
+            this.lblTermStart.Size = new System.Drawing.Size(146, 20);
             this.lblTermStart.TabIndex = 12;
             this.lblTermStart.Text = "Term Start Date";
             // 
@@ -191,16 +203,18 @@
             // 
             this.lblTermEnd.AutoSize = true;
             this.lblTermEnd.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTermEnd.Location = new System.Drawing.Point(336, 111);
+            this.lblTermEnd.Location = new System.Drawing.Point(538, 137);
+            this.lblTermEnd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTermEnd.Name = "lblTermEnd";
-            this.lblTermEnd.Size = new System.Drawing.Size(128, 18);
+            this.lblTermEnd.Size = new System.Drawing.Size(134, 20);
             this.lblTermEnd.TabIndex = 13;
             this.lblTermEnd.Text = "Term End Date";
             // 
             // mcTermStart
             // 
             this.mcTermStart.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mcTermStart.Location = new System.Drawing.Point(24, 138);
+            this.mcTermStart.Location = new System.Drawing.Point(71, 170);
+            this.mcTermStart.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.mcTermStart.MaxSelectionCount = 1;
             this.mcTermStart.Name = "mcTermStart";
             this.mcTermStart.TabIndex = 14;
@@ -208,16 +222,17 @@
             // mcTermEnd
             // 
             this.mcTermEnd.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mcTermEnd.Location = new System.Drawing.Point(283, 138);
+            this.mcTermEnd.Location = new System.Drawing.Point(473, 170);
+            this.mcTermEnd.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.mcTermEnd.MaxSelectionCount = 1;
             this.mcTermEnd.Name = "mcTermEnd";
             this.mcTermEnd.TabIndex = 15;
             // 
             // newTerm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 557);
+            this.ClientSize = new System.Drawing.Size(1282, 753);
             this.Controls.Add(this.mcTermEnd);
             this.Controls.Add(this.mcTermStart);
             this.Controls.Add(this.lblTermEnd);
@@ -235,7 +250,8 @@
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.lblName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = Properties.Resources.Marimba_logo;
+            this.Icon = global::Marimba.Properties.Resources.Marimba_logo;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "newTerm";
             this.ShowInTaskbar = false;
