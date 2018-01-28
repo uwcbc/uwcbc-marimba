f249685aaee69428fe4c2b98cbcca39e46f02ce9
Nathan <Nathan@NATHAN>
2018-01-28 13:13:14 -0500

    Updated comment.

--- Marimba/Program.cs
+++ Marimba/Program.cs
@@ -16,9 +16,10 @@
         public static void Main(string[] args)
         {
             // This fixes when one is trying to open a .mrb file and the file name doesn't get passed in
+            // Comment out the line if debugging as it throws an Argument Null Exception
             if (args == null || args.Length == 0)
             {
-                args = AppDomain.CurrentDomain.SetupInformation.ActivationArguments‌​.ActivationData.Select(s => new Uri(s).LocalPath).ToArray();
+                //args = AppDomain.CurrentDomain.SetupInformation.ActivationArguments‌​.ActivationData.Select(s => new Uri(s).LocalPath).ToArray();
             }
 
             Application.EnableVisualStyles();
