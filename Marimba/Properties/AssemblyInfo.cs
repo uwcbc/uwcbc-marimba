4b33e2b66ca8743394539eabd1306c5d32070ad1
Nathan <Nathan@NATHAN>
2020-08-30 16:18:51 -0400

    Change version and copyright year.

--- Marimba/Properties/AssemblyInfo.cs
+++ Marimba/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("UW Concert Band Club")]
 [assembly: AssemblyProduct("Marimba")]
-[assembly: AssemblyCopyright("Copyright ©  2013-2019 UW Concert Band Club")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2020 UW Concert Band Club")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.5")]
+[assembly: AssemblyVersion("2.3.1.1")]
 [assembly: AssemblyFileVersion("2.2.2.0")]
